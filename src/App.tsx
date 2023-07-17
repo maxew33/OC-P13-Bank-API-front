ed0ab4c421207455a74d9aff06ac9e9dacb72d10
Maxime Malfilatre <maxime.malfilatre@gmail.com>
2023-07-17 08:02:01 +0200

    :truck: [Router] add outlet

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
+import { Outlet } from 'react-router-dom'
 import Footer from './components/Footer'
 import Header from './components/Header'
-import Router from './router/Router'
 
 import './style/style.css'
 
@@ -8,7 +8,9 @@ function App() {
     return (
         <>
             <Header />
-            <Router />
+            <>
+                <Outlet />
+            </>
             <Footer />
         </>
     )
--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,52 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
-import { BrowserRouter } from 'react-router-dom'
+import {
+    BrowserRouter,
+    RouterProvider,
+    createBrowserRouter,
+} from 'react-router-dom'
 
 // REDUX
 import { Provider } from 'react-redux'
 import store from './utils/store.ts'
+import Index from './pages/Index.tsx'
+import SignIn from './pages/SignIn.tsx'
+import User from './pages/User.tsx'
+import Error from './pages/Error.tsx'
+
+import './style/style.css'
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <Error />,
+        children: [
+            {
+                path: '',
+                element: <Index />,
+            },
+            {
+                path: '/sign-in',
+                element: <SignIn />,
+            },
+            {
+                path: '/user',
+                element: <User />,
+            },
+            {
+                path: '*',
+                element: <Error />,
+            },
+        ],
+    },
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <Provider store={store}>
         <React.StrictMode>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
+            <RouterProvider router={router} />
         </React.StrictMode>
     </Provider>
 )
